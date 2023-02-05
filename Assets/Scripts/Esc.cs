f7b8fad190c60a63a5d3e558b05d93d9e4bdd562
robobruser <paulbruzzi@gmail.com>
2023-02-04 19:59:01 -0500

    Esc

---
+++ Assets/Scripts/Esc.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using System.Collections;
+ 
+// Quits the player when the user hits escape
+ 
+public class Esc : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)){
+            Application.Quit();
+        }
+    }
+}
